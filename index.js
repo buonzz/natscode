95337079938118e31f661911f6d5209f25614ad7
buonzz <buonzz@gmail.com>
2016-01-25 01:39:41 +0800

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+function natscode(){
+	return getCookie('nats');
+}
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+module.exports = natscode;
