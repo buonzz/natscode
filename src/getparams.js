4db0f9b4b4a6fef52ec2cb85e57851056383934a
buonzz <buonzz@gmail.com>
2018-04-22 11:42:48 +0800

    failing test

--- src/getparams.js
+++ src/getparams.js
@@ -5,7 +5,6 @@ var getnatscode = require('./getnatscode');
 function getparams(options){
 
 	var raw_params = qsf.parse(window.location.search);
-	var params = {};
 	var natscode_value = getnatscode(options);
 
 	if(natscode_value != null){
@@ -20,7 +19,7 @@ function getparams(options){
 	// if there is no querystring, but there is either cookie or defaults natscode set
 	// return the querystring but with natscode on it
 	if(!utils.isEmpty(raw_params) || natscode_value != null)	
-		return "?" + utils.toQueryString(params);
+		return "?" + utils.toQueryString(raw_params);
 
 	return '';
 
